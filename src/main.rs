9540fdd7482c757185f553f074a04d4adca6c2cc
Nonprofessional <5117724+Nonprofessional@users.noreply.github.com>
2022-05-02 01:22:51 +0800

    feat: add the upload feature

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ struct ServerConfig {
 
 use axum::{
     body::Body,
+    extract::{FromRequest, Multipart, RequestParts},
+    handler::Handler,
     http::{Request, StatusCode},
     middleware,
     response::{Html, IntoResponse},
@@ -48,19 +50,16 @@ async fn main() {
     let config = ServerConfig {
         work_path: root_path.clone(),
     };
-
+    let service = get_service(ServeDir::new(root_path))
+        .handle_error(|error: std::io::Error| async move {
+            (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Service Error: {}", error),
+            )
+        })
+        .post_service(upload_file.into_service());
     let app = Router::new()
-        .nest(
-            "",
-            get_service(ServeDir::new(root_path)).handle_error(
-                |error: std::io::Error| async move {
-                    (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Service Error: {}", error),
-                    )
-                },
-            ),
-        )
+        .nest("", service)
         .layer(middleware::from_fn(dir_handler))
         .layer(AddExtensionLayer::new(Arc::new(config)));
 
@@ -83,16 +82,22 @@ struct IndexTemplate {
     entry_list: Vec<EntryInfo>,
 }
 
+use hyper::Method;
+
 async fn dir_handler(
     req: Request<Body>,
     next: middleware::Next<Body>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
+    if req.method() == Method::POST {
+        let res = next.run(req).await;
+        return Ok(res);
+    }
+
     let mut work_path = match req.extensions().get::<Arc<ServerConfig>>() {
         Some(config) => config.work_path.to_owned(),
         None => return Err((StatusCode::INTERNAL_SERVER_ERROR, "Path Error".to_string())),
     };
     let uri = req.uri().clone();
-    println!("{:?}", uri);
 
     let res = next.run(req).await;
     if res.status() != StatusCode::NOT_FOUND {
@@ -102,7 +107,6 @@ async fn dir_handler(
     let uri = uri.to_string().trim_start_matches('/').to_owned();
     let decoded_uri = percent_encoding::percent_decode_str(&uri).decode_utf8_lossy();
     work_path.push(&*decoded_uri);
-    println!("{:?}", work_path);
     if work_path.is_dir() {
         if let Ok(entry_list) = list_entry(&work_path).await {
             let entry_list = IndexTemplate { entry_list };
@@ -118,6 +122,38 @@ async fn dir_handler(
     };
 
     Ok(res)
+}
+
+async fn upload_file(req: Request<Body>) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let mut work_path = match req.extensions().get::<Arc<ServerConfig>>() {
+        Some(config) => config.work_path.to_owned(),
+        None => return Err((StatusCode::INTERNAL_SERVER_ERROR, "Path Error".to_string())),
+    };
+    let uri = req.uri().clone();
+
+    let mut req_parts = RequestParts::new(req);
+    if let Ok(mut multipart) = Multipart::from_request(&mut req_parts).await {
+        while let Some(field) = multipart.next_field().await.unwrap() {
+            let file_name = field.file_name().unwrap().to_string();
+            let data = field.bytes().await.unwrap();
+
+            let uri = uri.to_string().trim_start_matches('/').to_owned();
+            let decoded_uri = percent_encoding::percent_decode_str(&uri).decode_utf8_lossy();
+            work_path.push(&*decoded_uri);
+            work_path.push(file_name);
+
+            if let Ok(_) = fs::File::create(&work_path).await {
+                if let Ok(_) = fs::write(&work_path, &data).await {
+                    return Ok(());
+                };
+            };
+        }
+    };
+
+    Err((
+        StatusCode::INTERNAL_SERVER_ERROR,
+        "Upload Error".to_string(),
+    ))
 }
 
 async fn list_entry(path: &PathBuf) -> std::io::Result<Vec<EntryInfo>> {
